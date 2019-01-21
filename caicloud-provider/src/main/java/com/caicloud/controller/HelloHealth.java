581f9ab152ea8b064e229a59fd3841eb4d6ea342
Shilei Sun <sunshilei@caicloud.io>
2019-01-21 12:25:18 +0800

    Update HelloHealth.java

--- caicloud-provider/src/main/java/com/caicloud/controller/HelloHealth.java
+++ caicloud-provider/src/main/java/com/caicloud/controller/HelloHealth.java
@@ -20,7 +20,15 @@ public class HelloHealth {
     public static final OkHttpClient httpClient = new OkHttpClient();
 
     @GetMapping("/www/{name}")
-    public String health(@PathVariable("name") String name) throws IOException {
+    public String health(@PathVariable("name") String name,
+                         @HeaderParam("end-user") String user,
+                         @HeaderParam("x-request-id") String xreq,
+                         @HeaderParam("x-b3-traceid") String xtraceid,
+                         @HeaderParam("x-b3-spanid") String xspanid,
+                         @HeaderParam("x-b3-parentspanid") String xparentspanid,
+                         @HeaderParam("x-b3-sampled") String xsampled,
+                         @HeaderParam("x-b3-flags") String xflags,
+                         @HeaderParam("x-ot-span-context") String xotspan) throws IOException {
         // 101110101
         name = "http://www.weather.com.cn/data/sk/"+name+".html";
         Request request = new Request.Builder()
@@ -31,7 +39,15 @@ public class HelloHealth {
     }
 
     @GetMapping("/health/{name}")
-    public String www(@PathVariable("name") String name){
+    public String www(@PathVariable("name") String name,
+                      @HeaderParam("end-user") String user,
+                     @HeaderParam("x-request-id") String xreq,
+                     @HeaderParam("x-b3-traceid") String xtraceid,
+                     @HeaderParam("x-b3-spanid") String xspanid,
+                     @HeaderParam("x-b3-parentspanid") String xparentspanid,
+                     @HeaderParam("x-b3-sampled") String xsampled,
+                     @HeaderParam("x-b3-flags") String xflags,
+                     @HeaderParam("x-ot-span-context") String xotspan) ){
 
         return "provider参数: "+name;
     }
